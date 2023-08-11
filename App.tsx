e835561e60d05f5c696b84680ca104952274f653
manuz7 <bassettonavesmanu@gmail.com>
2023-08-11 12:25:22 -0300

    atualização

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StatusBar, SafeAreaView } from 'react-native';
+import Carrinho from './src/pages/Carrinho';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Word</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+       <StatusBar/>
+   <Carrinho/>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
