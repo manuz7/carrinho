8edc3a79f58d0611afa5dcbe60bfcebebd25ae68
manuz7 <bassettonavesmanu@gmail.com>
2023-08-18 12:23:49 -0300

    Atualizando Carrinha 18/08

--- App.tsx
+++ App.tsx
@@ -1,7 +1,24 @@
 import { View, StatusBar, SafeAreaView } from 'react-native';
 import Carrinho from './src/pages/Carrinho';
+import{useFonts,
+       Montserrat_400Regular,
+       Montserrat_700Bold,
+       Montserrat_400Regular_Italic
+} from '@expo-google-fonts/montserrat';
 
 export default function App() {
+
+  const[fontCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+    "MontserratItalic": Montserrat_400Regular_Italic
+  })
+
+  if (!fontCarregada) {
+    <View/>
+  }
+
+  
   return (
     <SafeAreaView>
        <StatusBar/>
--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,8 +1,84 @@
 import React from 'react';
-import{ Image } from 'react-native';
+import{ StyleSheet, Image, Dimensions, Text, View } from 'react-native';
 
 import topo from '../../../assets/topo.png';
+import logo from '../../../assets/frutas/logo.png';
+
+const width = Dimensions.get('screen').width;
 
 export default function Carrinho (){
-    return <Image source= {topo}/>
+    return <>
+    <Image source={topo} style={estilos.topo} />
+    <Text style= {estilos.titulo}>Detalhe do Carrinho</Text>
+    <View style= {estilos.carrinho}>
+        <Text style= {estilos.titulocarrinho}>Carrinho de Compras</Text>
+        <View style=  {estilos.fazenda}>
+        <Image style= {estilos.imagemFazenda} source={logo}/>
+        <Text style= {estilos.nomeFazenda}>Fazenda Etec Cidade do Livro</Text>
+        </View>
+        <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente 
+            da fazenda direto para sua cozinha
+        </Text>
+        <Text>R$ 40,00</Text>
+       </View>
+       </>
 }
+
+
+const estilos = StyleSheet.create({
+      topo: {
+          width: "100%",
+          height: 578 / 768 * width
+      },
+      titulo: {
+          fontSize: 24,
+          fontWeight: "bold",
+          lineHeight: 26,
+          width: "100%",
+          textAlign: "center",
+          color: "white",
+          position: "absolute",
+          padding: 16
+
+      },
+         titulocarrinho:  {
+             fontSize : 26,
+             lineHeight: 42,
+             color: "#464646"
+             fontFamily: "MontserratBold"
+         },
+
+         carrinho: {
+             paddingVertical: 8,
+             paddingHorizontal: 16
+         },
+
+         nomeFazenda: {
+           fontSize: 16,
+           lineHeight: 26,
+           marginLeft: 12,
+           fontWeight: "bold"
+          },
+
+          imagemFazenda: {
+              width: 32,
+              height: 32
+          },
+
+          fazenda:{
+              flexDirection: "row",
+              paddingVertical: 12
+          },
+          descricao:{
+              color: "A3A3A3",
+              fontStyle: "italic"
+          },
+
+          preco: {
+             fontSize: 26,
+             lineHeight: 42,
+             color: "#2A9F85",
+             marginTop: 8
+
+          }
+    })
