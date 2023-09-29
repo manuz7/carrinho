5031561100fa884defddf4ee3ed8f92234d8f800
manuz7 <bassettonavesmanu@gmail.com>
2023-09-29 12:04:28 -0300

    atualização

--- src/pages/Carrinho/components/Detalhe.tsx
+++ src/pages/Carrinho/components/Detalhe.tsx
@@ -3,19 +3,25 @@ import {Text, Image, View, StyleSheet} from 'react-native';
 
 import logo from '../../../../assets/frutas/logo.png';
 
+type Props ={
+    nome: string,
+    nomeFazenda: string,
+    descricao:string,
+    preco:string
+}
 
-export default function Detalhe() {
+export default function Detalhe({nome, nomeFazenda, descricao, preco: Props}) {
     return<>
 
-         <Text style= {estilos.titulocarrinho}>Carrinho de Compras</Text>
+            <Text style= {estilos.titulocarrinho}>{nome}</Text>
         <View style=  {estilos.fazenda}>
         <Image style= {estilos.imagemFazenda} source={logo}/>
-        <Text style= {estilos.nomeFazenda}>Fazenda Etec Cidade do Livro</Text>
+        <Text style= {estilos.nomeFazenda}>{nomeFazenda}</Text>
         </View>
-        <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente 
-            da fazenda direto para sua cozinha
+        <Text style={estilos.descricao}>
+            {descricao}
         </Text>
-        <Text>R$ 40,00</Text>
+        <Text style={estilos.preco}>{preco}</Text>
 
 </>
 
--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -11,6 +11,10 @@ export default function Carrinho (){
   <Topo/>
     <View style= {estilos.carrinho}>
        <Detalhe/>
+       nome={carrinho.detalhes.nome}
+       nomeFazenda={carrinho.detalhes.nomeFazenda}
+       descricao={Carrinho.detalhes.descricao}
+       preco={carrinho.detalhes.preco}
        </View>
        </>
 }
@@ -21,4 +25,5 @@ const estilos = StyleSheet.create({
         paddingVertical: 8,
         paddingHorizontal: 16
     }
+
 })
